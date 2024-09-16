e6cde1e0151302ab47a937a107371c7af2e14810
Rob Palkowski <dopeghoti@gmail.com>
2024-09-16 18:20:05 +0000

    Add example API calls and a template for future examples

---
+++ REST_API_Examples/README.md
@@ -0,0 +1,4 @@
+# REST API Examples
+
+Here I compile a list of working examples for hitting the REST API.  Each file will work as a singleton,
+and each filename corresponsds to its respective API endpoint.
---
+++ REST_API_Examples/VerifyAuthenticationToken.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+# We don't care that we're almost definitely hitting a self-signed certificate
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+myServer = 'satisfactory.example.com'
+myPort = 7777
+myAPIVersion = 1
+myToken = None # Replace with an exact string copy of your Bearer Token.  Do not include the 'Bearer: ' prefix.
+
+try:
+    r = requests.post (
+        f'https://{myServer}:{myPort}/api/v{myAPIVersion}',
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {myToken}',
+        },
+        json = {
+            "function": "VerifyAuthenticationToken",
+        },
+        verify = False # If assuming self-signed certificates
+    )
+    try:
+        print( r.json()['data'] )
+    except:
+        print( r.status_code )
+        print( r.text )
+except requests.exceptions.RequestException as e:
+    print( f'Error: {e}' )
